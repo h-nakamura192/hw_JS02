ffa3ef663bc42872a694f9e2523d469f354aa36d
nakamura <gen.2polym.hn@gmail.com>
2020-12-18 09:02:32 +0000

    modify1

---
+++ JS02.js
@@ -0,0 +1,51 @@
+
+let btn = document.getElementById('btn');
+
+
+
+btn.addEventListener('click', function() {
+
+  let InputFizzNum = document.getElementById("InputFizzNum").value;
+    let InputBuzzNum = document.getElementById("InputBuzzNum").value;
+  
+  //console.log(Number.islnteger("2"));
+
+  const FizzNum = Number(InputFizzNum);
+  const BuzzNum = Number(InputBuzzNum);
+
+
+
+  if(Number.isInteger(FizzNum) && Number.isInteger(BuzzNum)){
+
+    let FizzBuzz = FizzNum * BuzzNum;
+
+    for(let number = 1; number <= 99; number++){
+      let content;
+      if(number % FizzBuzz == 0){
+        content = 'FizzBuzz'  + number;
+        let p = document.createElement('p');  
+        p.innerHTML = content;
+        document.body.appendChild(p);
+      }else if(number % FizzNum == 0){
+        content = 'Fizz' + number;
+        let p = document.createElement('p');  
+        p.innerHTML = content;
+        document.body.appendChild(p);
+      }else if(number % BuzzNum == 0){
+        content = 'Buzz' + number;
+        let p = document.createElement('p');  
+        p.innerHTML = content;
+        document.body.appendChild(p);
+      }
+    }
+  }else{
+    let content;
+    content = '整数値を入力してください'
+    let p = document.createElement('p');  
+    p.innerHTML = content;
+    document.body.appendChild(p);
+  }
+})
+
+
+
