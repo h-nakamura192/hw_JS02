50e7e7f9a504cd937061efde409003e184ddbfb1
nakamura <gen.2polym.hn@gmail.com>
2020-12-24 05:35:09 +0000

    mpdify3: let => const, add space

--- assets/JS02.js
+++ assets/JS02.js
@@ -16,35 +16,35 @@ btn.addEventListener('click', function() {
   const output = document.getElementById("output");
   output.innerHTML = "";
 
-  if(Number.isInteger(FizzNum) && 
+  if (Number.isInteger(FizzNum) && 
       Number.isInteger(BuzzNum) &&
-        (InputFizzNum !== "" && InputBuzzNum !== "")){
+        (InputFizzNum !== "" && InputBuzzNum !== "")) {
 
     const FizzBuzz = FizzNum * BuzzNum;
 
-    for(let number = 1; number <= 99; number++){
-      let content;
-      if(number % FizzBuzz == 0){
-        content = 'FizzBuzz'  + number;
-        let p = document.createElement('p');  
+    for (let number = 1; number <= 99; number++) {
+
+      if (number % FizzBuzz === 0) {
+        const content = 'FizzBuzz'  + number;
+        const p = document.createElement('p');  
         p.innerHTML = content;
         output.appendChild(p);
-      }else if(number % FizzNum == 0){
-        content = `Fizz ${number}`;
-        let p = document.createElement('p');  
+      } else if (number % FizzNum === 0) {
+        const content = `Fizz ${number}`;
+        const p = document.createElement('p');  
         p.innerHTML = content;
         output.appendChild(p);
-      }else if(number % BuzzNum == 0){
-        content = `Buzz ${number}`;
-        let p = document.createElement('p');  
+      } else if (number % BuzzNum === 0) {
+        const content = `Buzz ${number}`;
+        const p = document.createElement('p');  
         p.innerHTML = content;
         output.appendChild(p);
       }
     }
     
-  }else{
-    content = '整数値を入力してください';
-    let p = document.createElement('p');  
+  } else {
+    const content = '整数値を入力してください';
+    const p = document.createElement('p');  
     p.innerHTML = content;
     output.appendChild(p);
   }
